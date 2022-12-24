eab0c868445ca7904d8ca479f23e551360c67066
Guanwenlei <112093007+Guanwenlei@users.noreply.github.com>
2022-12-24 15:14:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'eNlMfdsCt23GIq_qYOFySX34_cgPznBJyCj-Rrww66A',
+  CALLBACK_TEMPLATE_ID: 'dh9SNDLZA29GJAA6ynOiM7L8KO2Tza-IHCr35QqKj4I',
 
   CALLBACK_USERS: [
     {
