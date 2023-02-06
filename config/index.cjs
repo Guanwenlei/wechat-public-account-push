e1565ed81ae9b8bebbd6b08a5c3deba2f903dc0c
Guanwenlei <112093007+Guanwenlei@users.noreply.github.com>
2023-02-06 14:34:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,38 +13,38 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '9cc8db4594a8edb2dfc5b61dc92cdbc2',
 
-  PROVINCE: '湖南',
-  CITY: '岳阳',
+  PROVINCE: '北京',
+  CITY: '通州',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '老婆',
+      name: '自己',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'owt676ROmN8xvKpI3jCBTQpFLkq4',
+      id: 'owt676WD7rCafP9RZ7iCKYmKwd9I',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'bTFOw2JI3GEDIcpJkeB3jUKLfGe7Gzp-g9N8KM_OR24',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '10-01',
+      horoscopeDate: '08-20',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '小公主', year: '2002', date: '10-01',
+          type: '*生日', name: '猪儿', year: '2002', date: '08-20',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '公主的骑士', year: '1999', date: '04-21',
+          type: '生日', name: '帅哥', year: '1999', date: '04-21',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2023', date: '01-10',
+          type: '节日', name: '相识纪念日', year: '2022', date: '08-18',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-01-10' },
+        { keyword: 'love_day', date: '2022-08-18' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2023-01-10' },
+        { keyword: 'marry_day', date: '2022-08-18' },
       ],
     },
   ],
